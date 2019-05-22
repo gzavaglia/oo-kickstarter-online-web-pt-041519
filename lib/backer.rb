8e593cefd12ba4afb61ff4678ea93246cef3a888
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-22 01:01:05 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,9 @@
 class Backer
-  attr_reader :backed_projecs
+  attr_reader :backed_projects
   attr_accessor :name
   
   def initialize(b_name)
     @name = b_name
-    @backed_projecs = []
+    @backed_projects = []
   end #end init
 end #end class
