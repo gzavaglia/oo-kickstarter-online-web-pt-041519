16c85196ad9cf1548d27268a2f43da1abdf3d649
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-22 01:19:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -13,5 +13,12 @@ class Backer
     
   end #end back_project
   
+  # def artist_name=(name)
+  #   if (self.artist.nil?)
+  #     self.artist = Artist.new(name)
+  #   else
+  #     self.artist.name = name
+  #   end
+  # end
  
 end #end class
